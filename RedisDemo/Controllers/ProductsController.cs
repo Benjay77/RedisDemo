d9f1fd1ec91dad4c40c33799a6262ddd48fd98f6
Benjay·Shaw <benjay7@163.com>
2020-06-26 17:02:01 +0800

    1.添加产品模块 2. 做浏览量redis统计和按浏览量排序 3.以及最近浏览的5个产品

---
+++ RedisDemo/Controllers/ProductsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using RedisDemo.Data;
+using RedisDemo.Models;
+using RedisDemo.ViewModels;
+using StackExchange.Redis;
+
+namespace RedisDemo.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private readonly IConnectionMultiplexer _redis;
+
+        private readonly IDatabase _db;
+
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public ProductsController(ApplicationDbContext applicationDbContext,IConnectionMultiplexer redis)
+        {
+            _applicationDbContext = applicationDbContext;
+            _redis = redis;
+            _db = _redis.GetDatabase();
+        }   
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var products = await _applicationDbContext.Products.ToListAsync();
+            if (products!=null&&products.Count>0)
+            {
+                var vms = products.Select(x=>new ProductViewModel
+                { 
+                    Id = x.Id,
+                    Name = x.Name
+                }).ToList().OrderBy(a=>a.ViewCount);
+
+                RedisKey[] redisKeys = vms.Select(x => (RedisKey)$"Product:{x.Id}:Views").ToArray();
+                var viewCounts = await _db.StringGetAsync(redisKeys);
+
+                foreach (var item in vms)
+                {
+                    var id = item.Id;
+                    var key = $"Product:{id}:Views";
+                    var index = redisKeys.IndexOf(key);
+
+                    if (index>-1)
+                    {
+                        item.ViewCount = (int)viewCounts[index];
+                    }
+                }
+                return View("View",vms);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            Product product = await _applicationDbContext.Products.FirstAsync(x => x.Id == id);
+            if (product!=null)
+            {
+                await _db.StringIncrementAsync($"Product:{product.Id}:Views");
+                product.ViewCount = (int)await _db.StringGetAsync($"Product:{product.Id}:Views");
+
+                var viewKey = "recentViewedProducts";
+                var element = $"产品:{product.Name}({ product.Id})次数:{product.ViewCount}";
+
+                await _db.ListLeftPushAsync(viewKey, element);
+
+                return View(product);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> RecentViewedProducts()
+        {
+            var list = await _db.ListRangeAsync("recentViewedProducts", 0, 4);
+            await _db.ListTrimAsync("recentViewedProducts", 0, 4);
+
+            return View(list);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Edit/5
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Delete/5
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- RedisDemo/Data/ApplicationDbContext.cs
+++ RedisDemo/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RedisDemo.Models;
 
 namespace RedisDemo.Data
 {
@@ -12,5 +13,6 @@ namespace RedisDemo.Data
             : base(options)
         {
         }
+        public DbSet<Product> Products { get; set; }
     }
 }
